0bfac2c05be99c1426ad9b40c3b61ae2b13534b8
Clairemuigua <muiguaclaire1@gmail.com>
2023-07-27 17:03:03 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// Additions  +
+let num1= 10
+let num2=20
+const sum=num1 + num2
+console.log(sum)
+// subtraction -
+const diff= num1 - num2
+console.log(diff)
+// division /
+const divide =num2 / num1
+console.log(divide)
+// multiplication
+const multiply =num1 * num2
+console.log(multiply)
+
+// modular/remainder
+const module=num2 % num1
+console.log(module)
+// exponentiation
+const exponent =num1** num2
+console.log(exponent)
+// increment
+const increase=num1++
+console.log (num1)
+// decrement
+const decrease =num2--
+console.log(num2)
+// Math medos
+// math.floor this runs down a number
+console.log(Math.floor(9.78))
+//math.random
+console.log(Math.random()* 5 )
+// math.ceil- rounds up a number
+console.log (Math.ceil(9.78))
+// math.min
+console.log(Math.min(num1,num2))
+// math.max
+console.log(Math.max(num1,num2))
+// math.pow
+console.log(Math.pow(num1,num2))
+// math.abs
+console.log(Math.abs(num1))
+// math.sqrt
+console.log(Math.sqrt(num2))
